cdffcf9866aaf01e55c15c4864876865a86bc016
Craig Sparks <sparks.craig@gmail.com>
2014-09-26 17:54:16 -0400

    Adding user agent to request

--- classes/proxy.php
+++ classes/proxy.php
@@ -2,17 +2,19 @@
 
 class Proxy {
 
+	protected $_user_agent = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36';
+
 	public function connect($url, $additional_options = array())
 	{
 		do
 		{
 			$response = NULL;
 			$success = FALSE;
-			$proxy = new Model_Proxy;
-			$proxy->find_oldest_used();
 
 			try
 			{
+				$proxy = new Model_Proxy;
+				$proxy->find_oldest_used();
 				$response = $this->do_request($proxy->get_full_host(), $url, $proxy->get_curl_auth(), $additional_options);
 				$success = TRUE;
 				$proxy->mark_used(TRUE, NULL, 'Success');
@@ -35,9 +37,11 @@ class Proxy {
 	protected function do_request($proxy_url, $url, $username_password = NULL, $additional_options = array())
 	{
 		$options = $additional_options +
-			array(CURLOPT_PROXY => $proxy_url,CURLOPT_PROXYUSERPWD => $username_password, CURLOPT_FAILONERROR => TRUE);
+			array(CURLOPT_PROXY => $proxy_url,CURLOPT_PROXYUSERPWD => $username_password, CURLOPT_FAILONERROR => TRUE,
+				  CURLOPT_USERAGENT => $this->_user_agent, CURLOPT_FOLLOWLOCATION => TRUE, CURLOPT_VERBOSE => TRUE);
 
 		$request = Request::factory($url);
+
 		$request->client()->options($options);
 
 		return $request;
